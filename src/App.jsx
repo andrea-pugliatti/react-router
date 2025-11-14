684b4cd68536da14060c1b6919ad52e0558fcf8a
andrea-pugliatti <andrea.pugliatti@outlook.it>
2025-11-14 11:39:25 +0100

    Add empty DefaultLayout

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import DefaultLayout from "./layouts/DefaultLayout";
 
 function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path="/" element={<h1>HomePage</h1>} />
-				<Route path="/about" element={<h1>AboutPage</h1>} />
-				<Route path="/products" element={<h1>Products</h1>} />
+				<Route element={<DefaultLayout />}>
+					<Route path="/" element={<h1>HomePage</h1>} />
+					<Route path="/about" element={<h1>AboutPage</h1>} />
+					<Route path="/products" element={<h1>Products</h1>} />
+				</Route>
 			</Routes>
 		</BrowserRouter>
 	);
