2e90a27857323cdc53b8cd1cc2fba14c9652467d
andrea-pugliatti <andrea.pugliatti@outlook.it>
2025-11-14 11:36:09 +0100

    Add BrowserRouter to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 function App() {
-	return <></>;
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<h1>HomePage</h1>} />
+				<Route path="/about" element={<h1>AboutPage</h1>} />
+				<Route path="/products" element={<h1>Products</h1>} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
