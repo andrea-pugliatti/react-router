4678fe3360db35b9574430c319be820014ced13e
andrea-pugliatti <andrea.pugliatti@outlook.it>
2025-11-14 12:29:03 +0100

    Add fetch to ProductsPage

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,3 +1,32 @@
+import { useEffect, useState } from "react";
+
 export default function ProductsPage() {
-	return <></>;
+	const productsEndpoint = "https://fakestoreapi.com/products";
+
+	// {
+	// 	id: 1,
+	// 	title: "",
+	// 	price: 1,
+	// 	description: "",
+	// 	category: "",
+	// 	image: "",
+	// 	rating: {
+	// 		rate: 1,
+	// 		count: 1,
+	// 	}
+	// }
+
+	const [products, setProducts] = useState([]);
+
+	const fetchProducts = () => {
+		fetch(productsEndpoint)
+			.then((response) => response.json())
+			.then(console.log)
+			.then(setProducts)
+			.catch((error) => console.error(error));
+	};
+
+	useEffect(fetchProducts, []);
+
+	return <div className="container"></div>;
 }
