c5f9bc05a681774f2f53aed31fdcba8b434ceef5
andrea-pugliatti <andrea.pugliatti@outlook.it>
2025-11-17 12:21:55 +0100

    Add fetch to Product Page

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,3 +1,32 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 export default function ProductPage() {
-	return <></>;
+	const { id } = useParams();
+	const [product, setProduct] = useState({});
+
+	const endpoint = `https://fakestoreapi.com/products/${id}`;
+
+	const fetchProduct = () => {
+		fetch(endpoint)
+			.then((response) => response.json())
+			.then(setProduct)
+			.catch((error) => console.error(error));
+	};
+
+	useEffect(() => {
+		fetchProduct();
+	}, []);
+
+	return (
+		<div className="container">
+			<img src={product.image} alt={product.title} />
+			<div>{product.title}</div>
+			<div>{product.category}</div>
+			<div>{product.description}</div>
+			<div>{product.price}</div>
+			{/* <div>{product.rating.count}</div>
+			<div>{product.rating.rate}</div> */}
+		</div>
+	);
 }
