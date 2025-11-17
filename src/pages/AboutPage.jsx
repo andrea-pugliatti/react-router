5697b5d268c0aca2ace3ebad25d07af7ad2123b4
andrea-pugliatti <andrea.pugliatti@outlook.it>
2025-11-17 16:12:10 +0100

    Add content and styles to About page and NotFound page

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,3 +1,23 @@
 export default function AboutPage() {
-	return <></>;
+	return (
+		<main>
+			<div className="about-page">
+				<div className="container">
+					<p>
+						NEGOZIO è nato nel 2015. Da un idea di{" "}
+						<span className="bold">Mario Rossi</span> e{" "}
+						<span className="bold">Michele Messina</span>
+						{". "}
+					</p>
+					<p>
+						Un piccolo laboratorio a Firenze, Italia con sedi a Londra e Parigi.
+						La nostra missione è fornire ai nostri clienti prodotti unici e di
+						alta qualità, curati nel design e realizzati nel pieno rispetto
+						dell'ambiente e dei diritti dei lavoratori. Vogliamo essere il ponte
+						tra l'artigiano e l'acquirente consapevole.
+					</p>
+				</div>
+			</div>
+		</main>
+	);
 }
--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -3,8 +3,10 @@ import { Link } from "react-router-dom";
 export default function NotFoundPage() {
 	return (
 		<div className="container">
-			<h2>404 Not Found</h2>
-			<Link to="/">Torna alla homepage</Link>
+			<div className="not-found">
+				<h2>404 Not Found</h2>
+				<Link to="/">Torna alla homepage</Link>
+			</div>
 		</div>
 	);
 }
